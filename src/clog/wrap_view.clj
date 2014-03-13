5dc77c6ad62730c765b9c44dbc7f4106d490b8e8
xvblack <xvblue@gmail.com>
2014-03-13 21:44:44 +0800

    merge server change

--- src/clog/wrap_view.clj
+++ src/clog/wrap_view.clj
@@ -17,62 +17,63 @@
 (defn wrap-view [content & {:keys [sidebars]}]
   (let [sidebars (if-not (vector? (first sidebars)) [sidebars] sidebars)]
     (page/html5
-   [:meta {:charset "utf-8"}]
-   [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
-   [:head
-    [:title "Clog: A simple blog"]
-    (build-widget :js-loader
-                  "http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha256.js")
-;;     [:script {:src "http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha256.js"}]
-    (build-widget :css-loader
-                  "/css/codemirror.css"
-                  "/css/style.css"
-                  "/css/fonts.css"
-                  )
-;;     [:link {:rel "stylesheet" :href "/css/codemirror.css"}]
-;;     [:link {:rel "stylesheet" :href "/css/style.css"}]
-;;     [:link {:rel "stylesheet" :href "/css/fonts.css"}]
-    (build-widget :js-loader
-                  "/js/codemirror.js"
-                  "/js/mode/markdown/markdown.js"
-                  "/js/continuelist.js"
-                  "/js/jquery.js"
-                  "/js/page.js"
-                  "/js/clog.js"
-                  "/js/clog-editor.js"
-                  "/js/react.js"
-                  "/js/JSXTransformer.js"
-                  "/js/inline-attach.js"
-                  "/js/codemirror.inline-attach.js")
-;;     [:script {:src "/js/codemirror.js"}]
-;;     [:script {:src "/js/mode/markdown/markdown.js"}]
-;;     [:script {:src "/js/jquery.js"}]
-;;     [:script {:src "/js/page.js"}]
-;;     [:script {:src "/js/clog.js"}]
-;;     [:script {:src "/js/continuelist.js"}]
-;;     [:script {:src "/js/clog-editor.js"}]
-;;     [:script {:src "/js/react.js"}]
-;;     [:script {:src "/js/JSXTransformer.js"}]
-    [:script {:type "text/jsx" :src "/js/picker.js"}]
-    ]
-   [:body
-    [:div#wrapper
-     [:div#header
-      [:a {:href "/"} [:div#logo]]
-      [:button#onlyButton.action.bluebtn
-       [:span.label "≡"]]]
-     [:div#contents content]
-     [:div#footer
-      [:p "I love sigsig. It is a dream!"]]]
-    [:div#sidebar
-     (map (fn [sw]
-            [:div.sidewidget sw])
-          sidebars)
-     [:div.sidewidget
-      [:h3 "Featured Posts"]
-      [:p " I feel alright! "]]
-     [:div.sidewidget
-      [:h3 "Featured Posts"]
-      [:p " I feel alright! "]]]
-    #_[:script {:src "/js/cljs.js"}]
-])))
+     [:meta {:charset "utf-8"}]
+     [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
+     [:meta {:name "viewport" :content "width=device-width,initial-scale=1, maximum-scale=2"}]
+     [:head
+      [:title "Clog: A simple blog"]
+      (build-widget :js-loader
+                    "http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha256.js")
+      ;;     [:script {:src "http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha256.js"}]
+      (build-widget :css-loader
+                    "/css/codemirror.css"
+                    "/css/style.css"
+                    "/css/fonts.css"
+                    )
+      ;;     [:link {:rel "stylesheet" :href "/css/codemirror.css"}]
+      ;;     [:link {:rel "stylesheet" :href "/css/style.css"}]
+      ;;     [:link {:rel "stylesheet" :href "/css/fonts.css"}]
+      (build-widget :js-loader
+                    "/js/codemirror.js"
+                    "/js/mode/markdown/markdown.js"
+                    "/js/continuelist.js"
+                    "/js/jquery.js"
+                    "/js/page.js"
+                    "/js/clog.js"
+                    "/js/clog-editor.js"
+                    "/js/react.js"
+                    "/js/JSXTransformer.js"
+                    "/js/inline-attach.js"
+                    "/js/codemirror.inline-attach.js")
+      ;;     [:script {:src "/js/codemirror.js"}]
+      ;;     [:script {:src "/js/mode/markdown/markdown.js"}]
+      ;;     [:script {:src "/js/jquery.js"}]
+      ;;     [:script {:src "/js/page.js"}]
+      ;;     [:script {:src "/js/clog.js"}]
+      ;;     [:script {:src "/js/continuelist.js"}]
+      ;;     [:script {:src "/js/clog-editor.js"}]
+      ;;     [:script {:src "/js/react.js"}]
+      ;;     [:script {:src "/js/JSXTransformer.js"}]
+      [:script {:type "text/jsx" :src "/js/picker.js"}]
+      ]
+     [:body
+      [:div#wrapper
+       [:div#header
+        [:a {:href "/"} [:div#logo]]
+        [:button#onlyButton.action.bluebtn
+         [:span.label "≡"]]]
+       [:div#contents content]
+       [:div#footer
+        [:p "I love sigsig. It is a dream!"]]]
+      [:div#sidebar
+       (map (fn [sw]
+              [:div.sidewidget sw])
+            sidebars)
+       [:div.sidewidget
+        [:h3 "Featured Posts"]
+        [:p " I feel alright! "]]
+       [:div.sidewidget
+        [:h3 "Featured Posts"]
+        [:p " I feel alright! "]]]
+      #_[:script {:src "/js/cljs.js"}]
+      ])))
